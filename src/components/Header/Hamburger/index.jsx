6c02952de8e4216a7c459fbda73f2a54d6e25a3a
Jon-PR0 <jonathan.almeida1793@gmail.com>
2022-06-02 17:50:21 -0300

    Correção do menu Hamburguer

--- src/components/Header/Hamburger/index.jsx
+++ src/components/Header/Hamburger/index.jsx
@@ -3,10 +3,12 @@ import './hamburger.css';
 
 function Hamburguer({ click }) {
   return (
-    <button onClick={click} className="md:hidden block cursor-pointer" aria-label="Abrir Menu" aria-haspopup="true" aria-controls="menu" aria-expanded="false" id="hamburger">
-      <span></span>
-      <span></span>
-      <span></span>
+    <button onClick={click} className="md:hidden block cursor-pointer">
+      <div id="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </button>
   );
 }
